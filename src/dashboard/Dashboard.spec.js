4b13f606b510eaf50418ddb7874025cf37827d44
abtorres <abtorres079@gmail.com>
2019-10-05 18:47:32 -0600

    Dashboard.spec.js completed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,36 @@
 // Test away
+// Test away!
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+import { fireEvent } from 'react-testing-library';
+afterEach(rtl.cleanup);
+
+describe('Dashboard', () => {
+    it('renders correctly', () => {
+        const wrapper = rtl.render(<Dashboard />);
+        expect(wrapper.baseElement).toMatchSnapshot();
+    });
+    it('defaults to open and unlocked', () => {
+        const { getByText } = rtl.render(<Dashboard />);
+        getByText(/Open/);
+        getByText(/Unlocked/);
+    });
+    it('cannot be open/closed when locked', () => {
+        const { getByText } = rtl.render(<Dashboard />);
+        const lockButton = getByText(/Lock Gate/);
+        const closeButton = getByText(/Close Gate/);
+        fireEvent.click(closeButton);
+        fireEvent.click(lockButton);
+        expect(closeButton.disabled).toBe(true);
+    });
+    it('shows the controls and display', () => {
+        const {getByText} = rtl.render(<Dashboard />);
+        getByText(/Open/);
+        getByText(/Unlocked/);
+        getByText(/Lock Gate/);
+        getByText(/Close Gate/);
+    });
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+// Test away!
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+afterEach(rtl.cleanup);
+
+describe('Display', () => {
+    it('displays correctly', () => {
+        const wrapper = rtl.render(<Display />);
+        expect(wrapper.baseElement).toMatchSnapshot();
+    })
+})
